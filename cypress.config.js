4d63d7968f65126bccaec4e1455307240eaebf5f
Laura <laurafariacastro@outlook.com>
2026-01-13 23:39:46 -0300

    Remove record, unset the port and add project id

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  port: 8080,
+  projectId: 'api',
+  // port: 8080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
